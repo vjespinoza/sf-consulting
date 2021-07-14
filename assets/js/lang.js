83ffb79f444226d9954e62daaa7a9c124467c124
vjespinoza <vjespinoza84@gmail.com>
2021-07-14 11:09:50 -0500

    Language detector implemented

---
+++ assets/js/lang.js
@@ -0,0 +1,10 @@
+const langDetector = () => {
+    const clientLang = navigator.language;
+
+    if (clientLang.includes("en")) {
+        // document.location.href = "http://www.klaudforce.com/en";
+        document.location.href = "http://127.0.0.1:5500/en";
+    }
+};
+
+langDetector();
