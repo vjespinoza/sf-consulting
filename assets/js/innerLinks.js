77bfbda4b99dc823ceadccc2ebee711ae8f9112e
vjespinoza <vjespinoza84@gmail.com>
2021-07-21 18:00:56 -0500

    English pages added

--- assets/js/innerLinks.js
+++ assets/js/innerLinks.js
@@ -1,34 +1,41 @@
-const baseURL = "http://127.0.0.1:5500/services/";
-const links = {
-    analytics: `${baseURL}analytics-reports.html`,
-    integration: `${baseURL}data-integration-migration.html`,
-    transformation: `${baseURL}digital-transformation.html`,
-    ecommerce: `${baseURL}e-commerce.html`,
-    marketing: `${baseURL}integrated-marketing.html`,
-    mobile: `${baseURL}mobile-apps.html`,
+const innerLink = () => {
+    // const baseURL = "http://127.0.0.1:5500/";
+    const baseURL = "http://www.klaudforce.com/";
+    const links = {
+        es: {
+            1: `${baseURL}services/digital-transformation.html`,
+            2: `${baseURL}services/data-integration-migration.html`,
+            3: `${baseURL}services/e-commerce.html`,
+            4: `${baseURL}services/analytics-reports.html`,
+            5: `${baseURL}services/integrated-marketing.html`,
+            6: `${baseURL}services/mobile-apps.html`,
+        },
+        en: {
+            1: `${baseURL}en/services/digital-transformation.html`,
+            2: `${baseURL}en/services/data-integration-migration.html`,
+            3: `${baseURL}en/services/e-commerce.html`,
+            4: `${baseURL}en/services/analytics-reports.html`,
+            5: `${baseURL}en/services/integrated-marketing.html`,
+            6: `${baseURL}en/services/mobile-apps.html`,
+        },
+    };
+
+    // document.location.href = path;
+    let targetList = Array.from(
+        document.querySelectorAll('[id ^= "service-card"]')
+    );
+
+    targetList.map((t) => {
+        t.addEventListener("click", (e) => {
+            let targetID = e.currentTarget.id.slice(13, 14);
+
+            if (e.currentTarget.id.includes("es")) {
+                document.location.href = links.es[targetID];
+            } else {
+                document.location.href = links.en[targetID];
+            }
+        });
+    });
 };
 
-const innerLink = (path) => {
-    // document.location.href(path);
-    document.location.href = path;
-    console.log(path);
-};
-
-document
-    .getElementById("service-card-1")
-    .addEventListener("click", () => innerLink(links.transformation));
-document
-    .getElementById("service-card-2")
-    .addEventListener("click", () => innerLink(links.integration));
-document
-    .getElementById("service-card-3")
-    .addEventListener("click", () => innerLink(links.ecommerce));
-document
-    .getElementById("service-card-4")
-    .addEventListener("click", () => innerLink(links.analytics));
-document
-    .getElementById("service-card-5")
-    .addEventListener("click", () => innerLink(links.marketing));
-document
-    .getElementById("service-card-6")
-    .addEventListener("click", () => innerLink(links.mobile));
+innerLink();
